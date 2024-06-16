71df1633c3f7b1b2543997af0d19876cad3f5f45
sivaneshj <sivaneshwaran03@gmail.com>
2024-06-16 13:53:22 +0530

    version 1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const lines = ()=>{
     while(true){
         const numofline = prompt("enter the number of lines to bet (1-3): ");
         const line = parseInt(numofline);
-        if(isNaN(line) || line<= 0){
+        if(isNaN(line) || line<= 0 || line > 3){
             console.log("invalid input");
         }
         else{
@@ -70,4 +70,69 @@ const spin = ()=>{
     return spining;
 }
 
+const spintrans = (spinny)=>{
+    const reels = [];
+    for(let i=0;i<3;i++){
+        reels.push([]);
+        for(let j=0;j<3;j++){
+            reels[i].push(spinny[j][i]);
+        }
+    }
+    return reels;
+}
+const decore =(val2)=>{
+    for(const row of val2){
+        let display = "";
+        for(const [val,inrow] of row.entries()){
+            display+=inrow;
+            if(val != row.length-1){
+                display+=" | ";
+            }
+        }
+        console.log(display);
+    }
+}
+const winings=(nooflines,bettingAmount,val2)=>{
+    let winamount = 0;
+    for(let i=0;i<nooflines;i++){
+        const symbols = val2[i];
+        let win = true;
+        for(const sym of symbols){
+            if(sym != symbols[0]){
+                win= false;
+                break;
+            }
+        }
+        if(win){
+            winamount+= bettingAmount * variablesvalue[symbols[0]];
+        }
+    }
+    return winamount
 
+}
+const game =()=>{
+    let Amountdeposit = deposit();
+    console.log("deposited Amount $"+Amountdeposit);
+    while(true){
+        const nooflines = lines()
+        const bettingAmount = betting();
+        Amountdeposit -= bettingAmount*nooflines;
+        console.log("remaining Amount $"+Amountdeposit);
+        const val1 = spin();
+        const val2 = spintrans(val1);
+        decore(val2)
+        const wamount = winings(nooflines,bettingAmount,val2)
+        console.log("You won $"+ wamount.toString());  
+        Amountdeposit += wamount;
+        console.log("Total Amount $"+Amountdeposit);
+        if(Amountdeposit <=0){
+            console.log("ran out money");
+            break;
+        }
+        const ask =prompt("want to play again (y/n): ");
+        if(ask != "y"){
+            break;
+        };      
+    }
+}
+game();
